e77563a736ede3364871118d4f40d7e0d90a07b4
Daniel <danielsangelo@hotmail.com>
2020-11-15 20:04:18 -0300

    adding required to the product name and category name. in addition the data annotation range has been added for the product quantity

--- DIO_AspNETMVC/Models/Category.cs
+++ DIO_AspNETMVC/Models/Category.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DIO_AspNETMVC.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category name must be informed")]
         public string Name { get; set; }
 
         public List<Product> Products { get; set; }
--- DIO_AspNETMVC/Models/Product.cs
+++ DIO_AspNETMVC/Models/Product.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DIO_AspNETMVC.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Product name must be informed")]
         public string Name { get; set; }
+        [Range(0, 9999, ErrorMessage = "Quantity must be between 0 and 9999")]
         public double Quantity { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
